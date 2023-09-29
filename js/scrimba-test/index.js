5eabaf8e1b495a9d9d570d45c87df9a9e799c813
Terry vanker <Terry.vanker89@gmail.com>
2023-09-29 20:44:49 +0200

    update

---
+++ js/scrimba-test/index.js
@@ -0,0 +1,24 @@
+document.getElementById("count").innerText = 5;
+
+let count = 0;
+
+addEventListener.count;
+
+function increment() {
+  count = count + 1;
+  console.log("incremented by 1");
+  console.log("the new count value is: " + count);
+}
+
+increment();
+
+let newCountValue = count;
+
+increment();
+
+let welcomeEl = document.getElementById("welcome-el"); //hämtar element genom id ifrån html
+
+let name = "Terry";
+let greeting = "Welcome back, ";
+
+welcomeEl.innerText = greeting + name;
