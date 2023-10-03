6e36871e69227b4d5e6d9bd134d2218a511bad2a
Terry vanker <Terry.vanker89@gmail.com>
2023-10-03 12:52:34 +0200

    updated

--- js/quizgame/script.js
+++ js/quizgame/script.js
@@ -2,9 +2,10 @@ const prompt = require("prompt-sync")();
 
 console.log("Welcome to this computer hardware Quiz!");
 let count = 0;
+const totalQuestions = 3;
 
-const answer1 = prompt("What is the brain of the computer? ");
-const correct_answer1 = "CPU";
+const answer1 = prompt("Do i like pie? ");
+const correct_answer1 = "YES";
 
 if (answer1.toUpperCase() === correct_answer1) {
   console.log("you got it correct!");
@@ -29,3 +30,19 @@ if (answer2.toUpperCase() === correct_answer2) {
 console.log(count);
 
 console.log("The third question: ");
+
+const answer3 = prompt("Do i like pizza ");
+const correct_answer3 = "YES";
+
+if (answer3.toUpperCase() === correct_answer3) {
+  console.log("You got it correct again!");
+  console.log("Gj you got 1 more point!");
+  count++;
+} else {
+  console.log("you got it wrong");
+}
+
+const percent = Math.round((count / totalQuestions) * 100);
+
+console.log("you got ", count, "correct answers");
+console.log("your score is ", percent, " percentage");
---
+++ js/randomNrGuesser/script.js
@@ -0,0 +1,22 @@
+const prompt = require("prompt-sync")();
+
+const target = Math.round(Math.random() * 100);
+
+let guesses = 0;
+console.log(target);
+
+while (true) {
+  guesses++;
+  const guess = Number(prompt("Guess the number (0-100): "));
+
+  if (guess > target) {
+    console.log("your guess is to high");
+  } else if (guess < target) {
+    console.log("your guess is to low");
+  } else {
+    console.log("Correct answer!");
+    break;
+  }
+}
+
+console.log("It took you", guesses, "guesses to get it right, gj");
