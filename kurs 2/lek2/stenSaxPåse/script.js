c2ffcdaf0ca1f667c747eaac3471dc3723ea419c
Terry vanker <Terry.vanker89@gmail.com>
2023-10-13 08:48:22 +0200

    stensaxpåse funkar

--- kurs 2/lek2/stenSaxPåse/script.js
+++ kurs 2/lek2/stenSaxPåse/script.js
@@ -1,14 +1,41 @@
-const btnElem = document.querySelector("#btn");
+const btnElemSten = document.querySelector("#btn-sten");
+const btnElemSax = document.querySelector("#btn-sax");
+const btnElemPåse = document.querySelector("#btn-påse");
 const resultElem = document.querySelector("#dittVal");
-const inputElem = document.querySelector("input");
 const compChoiceElem = document.querySelector("#compVal");
+
+/*    listener för bildknapp
+const btnImgSten = document.querySelector("#imageButton");
+btnImgSten.addEventListener("click", () => {
+  userGuess = "sten";
+  gameLogic(userGuess);
+});  */
+
+//const inputElem = document.querySelector("input");
+
+//const userGuess = inputElem.value;
 
 const computerChoices = ["sten", "sax", "påse"];
 const computersGuess =
   computerChoices[Math.floor(Math.random() * computerChoices.length)];
+let userGuess;
 
-btnElem.addEventListener("click", () => {
-  const userGuess = inputElem.value;
+btnElemSten.addEventListener("click", () => {
+  userGuess = "sten";
+  gameLogic(userGuess);
+});
+
+btnElemSax.addEventListener("click", () => {
+  userGuess = "sax";
+  gameLogic(userGuess);
+});
+
+btnElemPåse.addEventListener("click", () => {
+  userGuess = "påse";
+  gameLogic(userGuess);
+});
+
+function gameLogic() {
   resultElem.innerHTML = "Du valde " + userGuess;
   compChoiceElem.innerHTML = "Datorn valde " + computersGuess;
 
@@ -23,4 +50,4 @@ btnElem.addEventListener("click", () => {
   } else {
     resultElem.innerHTML = "Du vann";
   }
-});
+}
